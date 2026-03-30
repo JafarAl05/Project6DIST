0917bc7760b69240889d9f8fb2b2baaf7ff387de
Robeen <robeendoci1908@gmail.com>
2026-03-30 17:59:15 +0200

    2/3 done

--- naming-server/src/main/java/systemy/namingserver/FileStorage.java
+++ naming-server/src/main/java/systemy/namingserver/FileStorage.java
@@ -1,4 +1,27 @@
 package systemy.namingserver;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class FileStorage {
+    public void storeMap(HashMap<String, Integer> map) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String mapString = objectMapper.writeValueAsString(map);
+        FileWriter myWriter = new FileWriter("nameserver_map.txt");
+        myWriter.write(mapString);
+        myWriter.close();
+    }
+
+    public HashMap<String, Integer> loadMap(String filename) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        File file = new File(filename);
+        TypeReference<HashMap<String, Integer>> typeReference = new TypeReference<HashMap<String, Integer>> () {};
+        HashMap<String, Integer> outputMap = objectMapper.readValue(file, typeReference);
+        return outputMap;
+    }
 }
