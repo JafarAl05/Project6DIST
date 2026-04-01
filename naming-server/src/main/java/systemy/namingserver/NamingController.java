4fbee37a8f4a6e46155b47839a48460dddcc9b17
ToonVdK <101630967+ToonVdK@users.noreply.github.com>
2026-04-01 11:08:54 +0200

    Added Naming Server functionality

--- naming-server/src/main/java/systemy/namingserver/NamingController.java
+++ naming-server/src/main/java/systemy/namingserver/NamingController.java
@@ -1,4 +1,16 @@
 package systemy.namingserver;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class NamingController {
+
+    // Listen for GET requests asking for a file's location
+    @GetMapping("/files/{filename}")
+    public NodeResponse getFileLocation(@PathVariable("filename") String filename) {
+
+        // TODO: Later, call Robeen's MapManager and my Routing Algorithm here.
+
+        // For now, just return a hardcoded fake response so Role C can start testing!
+        return new NodeResponse(17, "192.168.0.4");
+    }
 }
--- naming-server/src/main/java/systemy/namingserver/NamingServerApp.java
+++ naming-server/src/main/java/systemy/namingserver/NamingServerApp.java
@@ -1,4 +1,11 @@
 package systemy.namingserver;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class NamingServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(NamingServerApp.class, args);
+    }
 }
