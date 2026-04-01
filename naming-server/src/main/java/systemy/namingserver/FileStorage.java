38bf2f1537d8cbe75e114c038c08060755e4aa19
Robeen <robeendoci1908@gmail.com>
2026-04-01 11:22:22 +0200

    mapmanager

--- naming-server/src/main/java/systemy/namingserver/FileStorage.java
+++ naming-server/src/main/java/systemy/namingserver/FileStorage.java
@@ -9,19 +9,24 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class FileStorage {
-    public void storeMap(HashMap<String, Integer> map) throws IOException {
+    static void storeMap(HashMap<Integer, String> map) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         String mapString = objectMapper.writeValueAsString(map);
-        FileWriter myWriter = new FileWriter("nameserver_map.txt");
+        FileWriter myWriter = new FileWriter("nameserver_map.json");
         myWriter.write(mapString);
         myWriter.close();
     }
 
-    public HashMap<String, Integer> loadMap(String filename) throws IOException {
+    static HashMap<Integer, String> loadMap(String filename) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         File file = new File(filename);
-        TypeReference<HashMap<String, Integer>> typeReference = new TypeReference<HashMap<String, Integer>> () {};
-        HashMap<String, Integer> outputMap = objectMapper.readValue(file, typeReference);
+        HashMap<Integer, String> outputMap;
+        if (file.exists()) {
+            TypeReference<HashMap<Integer, String>> typeReference = new TypeReference<HashMap<Integer, String>> () {};
+            outputMap = objectMapper.readValue(file, typeReference);
+        } else {
+            outputMap = new HashMap<Integer, String>();
+        }
         return outputMap;
     }
 }
